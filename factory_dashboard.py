d293c82aa9718510328adbf5d4a5d529a45bfefd
ManjunathBiradar-01 <manjunath.biradar@fayatec.com>
2025-08-21 18:57:04 +0530

    Update factory_dashboard.py

--- factory_dashboard.py
+++ factory_dashboard.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 # ---------- Settings ----------
 st.set_page_config(
-    page_title="Bomag Factory Relocation Dashboard",
+    page_title="Bomag SDMs Factory Production Relocation Dashboard",
     layout="wide"
 )
 
@@ -376,6 +376,7 @@ with st.expander("Show filtered data"):
     cols_to_show = [c for c in cols_to_show if c in filtered_df.columns]
 
     st.dataframe(filtered_df[cols_to_show].reset_index(drop=True)) 
+
 
 
 
