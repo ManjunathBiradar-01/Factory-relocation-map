60cffe8c27cc14404ad34686bd13e296ce452f7c
ManjunathBiradar-01 <manjunath.biradar@fayatec.com>
2025-08-22 10:22:49 +0530

    Update factory_dashboard.py

--- factory_dashboard.py
+++ factory_dashboard.py
@@ -120,7 +120,8 @@ else:
         st.stop()
 
 
-    st.error(f"Failed to load data from '{excel_path}'.\n\n{e}")
+   st.error(f"Failed to load data.\n\n{e}")
+
     st.stop()
 
 # ---------- UI (updated) ----------
@@ -391,6 +392,7 @@ with st.expander("Show filtered data"):
     cols_to_show = [c for c in cols_to_show if c in filtered_df.columns]
 
     st.dataframe(filtered_df[cols_to_show].reset_index(drop=True)) 
+
 
 
 
