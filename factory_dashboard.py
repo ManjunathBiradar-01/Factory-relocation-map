80e572e7208f9263d2b0433c04a02da649679e30
ManjunathBiradar-01 <manjunath.biradar@fayatec.com>
2025-09-09 14:17:47 +0530

    Update factory_dashboard.py

--- factory_dashboard.py
+++ factory_dashboard.py
@@ -254,12 +254,12 @@ st.markdown("""
     .kpi-title {
         font-size: 15px;
         font-weight: 300;
-        color: white;
+        color: black;
     }
     .kpi-value {
         font-size: 26px;
         font-weight: bolder;
-        color: white;
+        color: black;
         font-family: 'Roboto', monospace;
     }
     </style>
@@ -894,6 +894,7 @@ with st.expander("Show filtered data"):
     cols_to_show = [c for c in cols_to_show if c in filtered_df.columns]
 
     st.dataframe(filtered_df[cols_to_show].reset_index(drop=True)) 
+
 
 
 
